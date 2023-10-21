5a3ac833342d5e82f1a54ded1177c9baf11efc23
yveskalume <yvkalume@gmail.com>
2023-10-21 14:12:20 +0200

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,114 @@
+import de.undercouch.gradle.tasks.download.Download
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("de.undercouch.download") version "5.5.0"
+}
+
+android {
+    namespace = "com.yveskalume.instaglow"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.yveskalume.instaglow"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
+
+tasks.register<Download>("downloadESRGANModelFile") {
+    src("https://storage.googleapis.com/download.tensorflow.org/models/tflite/esrgan/ESRGAN.tflite")
+    dest("/src/main/assets/ESRGAN.tflite")
+    overwrite(false)
+}
+
+tasks.register<Download>("downloadTFLiteJARFile") {
+    download {
+        src("https://repo1.maven.org/maven2/org/tensorflow/tensorflow-lite/2.3.0/tensorflow-lite-2.3.0.aar")
+        dest("${project.rootDir}/libraries/tensorflow-lite-2.3.0.aar")
+        overwrite(false)
+        retries(5)
+    }
+}
+
+tasks.register<Download>("downloadTFLiteGPUDelegateJARFile") {
+    download {
+        src("https://repo1.maven.org/maven2/org/tensorflow/tensorflow-lite-gpu/2.3.0/tensorflow-lite-gpu-2.3.0.aar")
+        dest("${project.rootDir}/libraries/tensorflow-lite-gpu-2.3.0.aar")
+        overwrite(false)
+        retries(5)
+    }
+}
+
+tasks.register("fetchTFLiteLibs") {
+    doLast {
+        copy {
+            from(zipTree("${project.rootDir}/libraries/tensorflow-lite-2.3.0.aar"))
+            into("${project.rootDir}/libraries/tensorflowlite/")
+            include("headers/tensorflow/lite/c/*h")
+            include("headers/tensorflow/lite/*h")
+            include("jni/**/libtensorflowlite_jni.so")
+        }
+        copy {
+            from(zipTree("${project.rootDir}/libraries/tensorflow-lite-gpu-2.3.0.aar"))
+            into("${project.rootDir}/libraries/tensorflowlite-gpu/")
+            include("headers/tensorflow/lite/delegates/gpu/*h")
+            include("jni/**/libtensorflowlite_gpu_jni.so")
+        }
+    }
+}
